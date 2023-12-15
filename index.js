2c9d692a617b040dc2b80b3e60c0f8d26744050e
Saeed Moghadamzadeh <saeed.moqadam@gmail.com>
2023-12-16 00:32:27 +0100

    init

---
+++ index.js
@@ -0,0 +1,92 @@
+
+require('dotenv').config(); 
+const express = require('express')
+const puppeteer = require('puppeteer');
+var { Readability } = require('@mozilla/readability');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+const { basename } = require('path');
+
+
+const app = express()
+const port = 3000
+
+app.get('/', async (req, res) => {
+
+    let url = req.query.url;
+    console.log(req.query);
+    let response = await fetch(url);
+    let body = await response.text();
+    let article  = readable(body);
+    let filename = await toPdf(article.title, article.content);
+
+    sendEmail(filename);
+    res.send('webpage sent to kindle! ' + url)
+})
+
+
+const readable = (html) => {
+    const doc = new JSDOM(html);
+    var article = new Readability(doc.window.document).parse();
+    return article
+}
+
+const toPdf = async (title, html) => {
+    const browser = await puppeteer.launch();
+
+    const page = await browser.newPage();
+
+    await page.setContent(html);
+    const filename = 'docs/'+title+'.pdf';
+    await page.pdf({ path: filename, format: 'A5' });
+
+    await browser.close();
+
+    return filename;
+};
+
+app.listen(port, () => {
+    console.log(`webpage to kindle app listening on port ${port}`)
+})
+
+const sendEmail = (filename) => {
+
+// Create a transporter using Gmail SMTP
+const transporter = nodemailer.createTransport({
+    service: process.env.SMTP_SERVER,
+    auth: {
+      user: process.env.USERNAME,
+      pass: process.env.PASSWORD,
+    },
+  });
+  
+  // Email options
+  const mailOptions = {
+    from: process.env.FROM_EMAIL,
+    to: process.env.TO_EMAIL,
+    subject: 'convert',
+    text: 'convert',
+    attachments: [
+      {
+        filename: basename(filename),
+        content: fs.createReadStream(filename),
+      },
+      // Add more attachments as needed
+    ],
+  };
+  
+  // Send email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+      fs.rm(filename, ()=>{
+        console.log('file has been deleted')
+      });
+    }
+  });
+}
