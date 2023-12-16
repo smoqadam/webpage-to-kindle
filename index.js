c03efe71c023b2a17fc0b67a846af02705acfff3
Saeed Moghadamzadeh <saeed.moqadam@gmail.com>
2023-12-16 15:05:36 +0100

    add frontend, fix minor bugs

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
 
-require('dotenv').config(); 
+require('dotenv').config();
 const express = require('express')
 const puppeteer = require('puppeteer');
 var { Readability } = require('@mozilla/readability');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-
+const path = require('path');
 const nodemailer = require('nodemailer');
 const fs = require('fs');
 const { basename } = require('path');
+const { title } = require('process');
+const { json } = require('express');
 
 
 const app = express()
 const port = 3000
 
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
 
-    let url = req.query.url;
-    console.log(req.query);
-    let response = await fetch(url);
-    let body = await response.text();
-    let article  = readable(body);
-    let filename = await toPdf(article.title, article.content);
+app.get('/send', async (req, res) => {
 
-    sendEmail(filename);
-    res.send('webpage sent to kindle! ' + url)
+    try {
+        let url = req.query.url;
+        console.log(req.query);
+        let response = await fetch(url);
+        let body = await response.text();
+        let article = readable(body);
+        let filename = await toPdf(article.title, article.content);
+
+        sendEmail(filename);
+        res.send(JSON.stringify({
+            'error': false,
+            'message': 'success',
+        }));
+    } catch (e) {
+        console.log({e});
+        res.send(JSON.stringify({
+            'error': true,
+            "message": e.message,
+        }));
+    }
 })
 
 
@@ -40,7 +57,7 @@ const toPdf = async (title, html) => {
     const page = await browser.newPage();
 
     await page.setContent(html);
-    const filename = 'docs/'+title+'.pdf';
+    const filename = 'docs/' + title + '.pdf';
     await page.pdf({ path: filename, format: 'A5' });
 
     await browser.close();
@@ -54,39 +71,39 @@ app.listen(port, () => {
 
 const sendEmail = (filename) => {
 
-// Create a transporter using Gmail SMTP
-const transporter = nodemailer.createTransport({
-    service: process.env.SMTP_SERVER,
-    auth: {
-      user: process.env.USERNAME,
-      pass: process.env.PASSWORD,
-    },
-  });
-  
-  // Email options
-  const mailOptions = {
-    from: process.env.FROM_EMAIL,
-    to: process.env.TO_EMAIL,
-    subject: 'convert',
-    text: 'convert',
-    attachments: [
-      {
-        filename: basename(filename),
-        content: fs.createReadStream(filename),
-      },
-      // Add more attachments as needed
-    ],
-  };
-  
-  // Send email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-      fs.rm(filename, ()=>{
-        console.log('file has been deleted')
-      });
-    }
-  });
+    // Create a transporter using Gmail SMTP
+    const transporter = nodemailer.createTransport({
+        service: process.env.SMTP_SERVER,
+        auth: {
+            user: process.env.USERNAME,
+            pass: process.env.PASSWORD,
+        },
+    });
+
+    // Email options
+    const mailOptions = {
+        from: process.env.FROM_EMAIL,
+        to: process.env.TO_EMAIL,
+        subject: 'convert',
+        text: 'convert',
+        attachments: [
+            {
+                filename: basename(filename),
+                content: fs.createReadStream(filename),
+            },
+            // Add more attachments as needed
+        ],
+    };
+
+    // Send email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+            // fs.rm(filename, () => {
+            //     console.log('file has been deleted')
+            // });
+        }
+    });
 }
